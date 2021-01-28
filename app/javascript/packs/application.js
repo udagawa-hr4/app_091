080b9abfa8e110f448d145db7f9dd572c7dc7f07
udagawa-hr4 <hiro619881022@gmil.com>
2021-01-28 22:20:35 +0900

    ビューのテスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+// import '@fortawesome/fontawesome-free/js/all';
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+ 
+//= require jquery_ujs 
+//= require_tree .
+//= require_self 
+require("jquery")
+require("../kurukuru.js")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
